4b02c813a081250583359eb8cd542e41e1d8c3b5
wongxy <xiyao.wong@foxmail.com>
2020-05-25 19:50:26 +0800

    update

--- main.py
+++ main.py
@@ -1,31 +1,43 @@
 import json
+import os
 import time
 
 import requests
 
 session = requests.Session()
-repos = [
-    'pix',
-    'pix-2'
-]
-
+headers = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36'
+}
+repos = os.listdir('repos')
+print(repos)
+print()
 data = {
     'count': 0,
     'last_update': 0,
     'pics': []
 }
 
-try:
-    for repo in repos:
-        url = 'https://cdn.jsdelivr.net/gh/XiyaoWong/' + repo + '/data.json'
-        rep = session.get(url)
-        pics = [f'https://cdn.jsdelivr.net/gh/XiyaoWong/{repo}/pics/{pic}' for pic in rep.json()['pics']]
-        data['pics'].extend(pics)
+for repo in repos:
+    try:
+        try:
+            url = f'https://raw.githubusercontent.com/XiyaoWong/{repo}/master/data.json'
+            rep = session.get(url, headers=headers)
+            print(url, rep.status_code, repo, rep.json()['count'])
+            rep.raise_for_status()
+            pics = [f'https://cdn.jsdelivr.net/gh/XiyaoWong/{repo}/pics/{pic}' for pic in rep.json()['pics']]
+            data['pics'].extend(pics)
+        except Exception:
+            url = 'https://cdn.jsdelivr.net/gh/XiyaoWong/' + repo + '/data.json'
+            rep = session.get(url, headers=headers)
+            print(url, rep.status_code, repo, rep.json()['count'])
+            rep.raise_for_status()
+            pics = [f'https://cdn.jsdelivr.net/gh/XiyaoWong/{repo}/pics/{pic}' for pic in rep.json()['pics']]
+            data['pics'].extend(pics)
+    except Exception as e:
+        print(e)
 
-    data['count'] = len(data['pics'])
-    data['last_update'] = int(time.time())
+data['count'] = len(data['pics'])
+data['last_update'] = int(time.time())
 
-    with open('data.json', 'w') as f:
-        json.dump(data, f)
-except Exception as e:
-    print(e)
+with open('data.json', 'w') as f:
+    json.dump(data, f)
